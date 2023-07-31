91f7469ec8123272f8b0069196f31c3beebfd64c
Christian Lorenz <gromeck@gromeck.de>
2023-07-31 21:34:07 +0200

    working version

--- build.sh
+++ build.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 #
-
+#	do the live build
 #
-#	cleanup
-#
-sudo lb clean
 . ./common.sh
 
 #
 #	copy the DartScorer packages
 #
-cp -p ../DartScorer/dartscorer*.deb config/packages
-cp -p ../DartScorer-Voices/dartscorer-voices*.deb config/packages
+cp -p ../DartScorer/dartscorer*.deb config/packages/
+cp -p ../DartScorer-Voices/dartscorer-voices*.deb config/packages/
 
 #
 #	do the build
 #
-sudo time -p lb build
+sudo lb build
 
 #
 #	rename & show the result
 #
-du -h live-build*
+du -h $TITLE-*
 
 #
 #	EOF
